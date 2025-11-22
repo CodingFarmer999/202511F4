b89d1b673c55307a79987c64c74989abf3503ebe
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-11-22 14:03:00 +0800

    Contructor

--- 00-maven/src/main/java/com/course/CarMain.java
+++ 00-maven/src/main/java/com/course/CarMain.java
@@ -1,11 +1,12 @@
 package com.course;
 
 import com.course.model.Toyota;
+import com.course.model.ToyotaEngine;
 
 public class CarMain {
 
 	public static void main(String[] args) {
-		Toyota toyota = new Toyota();
+		Toyota toyota = new Toyota(new ToyotaEngine());
 		toyota.move();
 
 	}
--- 00-maven/src/main/java/com/course/model/Toyota.java
+++ 00-maven/src/main/java/com/course/model/Toyota.java
@@ -1,6 +1,12 @@
 package com.course.model;
 
 public class Toyota {
+	
+	private ToyotaEngine engine;
+	
+	public Toyota(ToyotaEngine engine) {
+		this.engine = engine;
+	}
 
 	public void move() {
 		// ToyotaEngine engine = new ToyotaEngine();
