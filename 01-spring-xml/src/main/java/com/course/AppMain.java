3c5f45ec30b2a4a53aebf913498945ebad418c52
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-11-22 16:13:54 +0800

    Java Configuration

---
+++ 01-spring-xml/src/main/java/com/course/AppMain.java
@@ -0,0 +1,18 @@
+package com.course;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.course.config.AppConfig;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		for (String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+	}
+
+}
