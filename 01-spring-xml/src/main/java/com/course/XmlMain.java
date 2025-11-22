cd95821c377909a08a431d4b8739a4b927be4b0e
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-11-22 15:26:54 +0800

    xml config

--- 01-spring-xml/src/main/java/com/course/XmlMain.java
+++ 01-spring-xml/src/main/java/com/course/XmlMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class XmlMain {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext();
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 
 	}
 
