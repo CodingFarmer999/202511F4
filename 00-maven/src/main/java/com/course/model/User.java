8b315e50ae9b62122816a432c99ec66cd8b0e3bf
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-11-22 13:54:05 +0800

    dependency

--- 00-maven/src/main/java/com/course/model/User.java
+++ 00-maven/src/main/java/com/course/model/User.java
@@ -9,20 +9,4 @@ public class User {
 	
 	private String name;
 
-//	public Integer getId() {
-//		return id;
-//	}
-//
-//	public void setId(Integer id) {
-//		this.id = id;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-
 }
