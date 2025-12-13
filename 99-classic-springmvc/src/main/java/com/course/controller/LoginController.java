63641226fd2af0c7a3196858f3709aadff068b49
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-13 10:06:48 +0800

    demo

--- 99-classic-springmvc/src/main/java/com/course/controller/LoginController.java
+++ 99-classic-springmvc/src/main/java/com/course/controller/LoginController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 // @Component
 public class LoginController {
-
+	// http://10.12.13.156/test/login
 	@GetMapping("/login")
 	public String login() {
+		// WEB-INF/views/loginSuccess.jsp
 		return "loginSuccess";
 	}
 }
