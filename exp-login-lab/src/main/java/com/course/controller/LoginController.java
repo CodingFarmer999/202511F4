adf9cc671a36c73abf3036f0d70e949628936404
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-13 16:20:27 +0800

    loginTest

--- exp-login-lab/src/main/java/com/course/controller/LoginController.java
+++ exp-login-lab/src/main/java/com/course/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.course.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -13,7 +14,11 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String loginAction() {
-		return "loginSuccess";
+	public String loginAction(@RequestParam String username, @RequestParam String password) {
+		if (username.equals("aaa") && password.equals("111")) {
+			return "loginSuccess";
+		} else {
+			return "loginFail";
+		}
 	}
 }
--- exp-login-lab/src/main/webapp/WEB-INF/views/loginFail.jsp
+++ exp-login-lab/src/main/webapp/WEB-INF/views/loginFail.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>登入失敗</h1>
 </body>
 </html>
