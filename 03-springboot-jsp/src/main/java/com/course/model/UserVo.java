a9b76ec1e974dc505d29d2393d3e997969b0a5a0
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-13 13:56:11 +0800

    UserVo

---
+++ 03-springboot-jsp/src/main/java/com/course/model/UserVo.java
@@ -0,0 +1,13 @@
+package com.course.model;
+
+import lombok.Data;
+
+@Data
+public class UserVo {
+	
+    private String nickname;
+    
+    private String phone;
+    
+    private String[] hobby;
+}
