1081d3b6df2d8e331fc2df712cadf2bf6ce6859f
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-13 10:47:34 +0800

    GetMapping

--- 03-springboot-jsp/src/main/java/com/course/controller/AppController.java
+++ 03-springboot-jsp/src/main/java/com/course/controller/AppController.java
@@ -1,13 +1,19 @@
 package com.course.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
 
+	@RequestMapping("/")
+	public String home() {
+		return "index2";
+	}
 	// URL: /login
-	@RequestMapping("/login")
+	// @RequestMapping(value = "/login", method = RequestMethod.GET)
+	@GetMapping("/login")
 	public String login() {
 		return "loginSuccess";
 	}
