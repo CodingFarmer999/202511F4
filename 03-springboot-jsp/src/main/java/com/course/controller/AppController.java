a2f7cee2557a2c26f683f9530ce403c583b1d128
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-13 10:32:00 +0800

    login

--- 03-springboot-jsp/src/main/java/com/course/controller/AppController.java
+++ 03-springboot-jsp/src/main/java/com/course/controller/AppController.java
@@ -1,7 +1,15 @@
 package com.course.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
 
 	// URL: /login
+	@RequestMapping("/login")
+	public String login() {
+		return "loginSuccess";
+	}
 	
 }
