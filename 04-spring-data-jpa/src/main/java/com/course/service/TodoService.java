04aae01c83d9b2c11e209ec68c02f610781db1c7
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-20 15:44:15 +0800

    dao

--- 04-spring-data-jpa/src/main/java/com/course/service/TodoService.java
+++ 04-spring-data-jpa/src/main/java/com/course/service/TodoService.java
@@ -1,11 +1,22 @@
 package com.course.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.course.dao.TodoDao;
+import com.course.entity.TodoEntity;
 import com.course.repository.TodoRepository;
 
 @Service
 public class TodoService {
 
 	private TodoRepository todoRepository;
+	
+	
+//	private TodoDao todoDao;
+	
+//	public List<TodoEntity> findAll() {
+//		return todoDao.findAll();
+//	}
 }
--- exp-login-lab/src/main/java/com/course/entity/UsersEntity.java
+++ exp-login-lab/src/main/java/com/course/entity/UsersEntity.java
@@ -1,5 +1,6 @@
 package com.course.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class UsersEntity {
 	
 	private String email;
 	
+	@Column(name = "img_name")
 	private String imgName;
 	
 }
