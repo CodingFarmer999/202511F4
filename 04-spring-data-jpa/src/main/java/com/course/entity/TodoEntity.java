2cd0885f2f6d0653c7e4d6aeb7089e4a324770d2
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-20 14:47:27 +0800

    Identity

--- 04-spring-data-jpa/src/main/java/com/course/entity/TodoEntity.java
+++ 04-spring-data-jpa/src/main/java/com/course/entity/TodoEntity.java
@@ -3,6 +3,8 @@ package com.course.entity;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class TodoEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String title;
