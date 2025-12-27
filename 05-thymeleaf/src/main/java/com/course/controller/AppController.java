1c4cf57dbb19c6ceaee3e47696b9de5916ee505e
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-27 16:04:57 +0800

    字串模板

--- 05-thymeleaf/src/main/java/com/course/controller/AppController.java
+++ 05-thymeleaf/src/main/java/com/course/controller/AppController.java
@@ -12,6 +12,18 @@ public class AppController {
 		model.addAttribute("title", "<span style=\"color:DodgerBlue;\">去唱卡拉ＯＫ吧</span>");
 		
 		model.addAttribute("title2", "<script>alert('你被攻擊了')</script>");
+		
+		model.addAttribute("param1", "半邊天");
+		
+		model.addAttribute("param2", "福利蓮");
 		return "app";
+	}
+	
+	public String test() {
+		String t = """
+				
+				""";
+		
+		return t;
 	}
 }
