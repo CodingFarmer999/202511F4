8a169345634d5d88fbde73c61e48c9d545afac5a
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2025-12-27 15:53:08 +0800

    utext

--- 05-thymeleaf/src/main/java/com/course/controller/AppController.java
+++ 05-thymeleaf/src/main/java/com/course/controller/AppController.java
@@ -9,7 +9,9 @@ public class AppController {
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("title", "我想要變有錢~");
+		model.addAttribute("title", "<span style=\"color:DodgerBlue;\">去唱卡拉ＯＫ吧</span>");
+		
+		model.addAttribute("title2", "<script>alert('你被攻擊了')</script>");
 		return "app";
 	}
 }
