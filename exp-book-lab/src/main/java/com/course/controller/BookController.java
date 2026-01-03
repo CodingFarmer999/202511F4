fcd863524ece707026f5910359bb92ec1a64e3c9
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-03 15:35:57 +0800

    login

--- exp-book-lab/src/main/java/com/course/controller/BookController.java
+++ exp-book-lab/src/main/java/com/course/controller/BookController.java
@@ -2,6 +2,7 @@ package com.course.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.course.service.BookService;
 
@@ -10,4 +11,10 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
+	
+	@PostMapping("/login")
+	public String login(String username, String password) {
+		// TODO: 檢查是否登入成功
+		return "loginSuccess";
+	}
 }
