313b4ee9bc28d00035c35040690edb906b25632f
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-10 11:19:09 +0800

    save image

--- exp-book-lab/src/main/java/com/course/service/BookService.java
+++ exp-book-lab/src/main/java/com/course/service/BookService.java
@@ -1,5 +1,6 @@
 package com.course.service;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,11 +20,14 @@ public class BookService {
 	@Autowired
 	private BookRepository bookRepo;
 	
+	@Autowired
+	private BookServiceHelper helper;
+	
 	public List<BookVo> getAllBook() {
 		List<BookEntity> books = bookRepo.findAll();
 		// Entity -> Vo
 		
-		return books.stream().map(entity -> convertToVo(entity)).collect(Collectors.toList());
+		return books.stream().map(entity -> helper.convertToVo(entity)).collect(Collectors.toList());
 		
 //		List<BookVo> voList = new ArrayList<>();
 //		for (BookEntity entity : books) {
@@ -44,42 +48,17 @@ public class BookService {
 		// 存進 DB
 		entity.setName(book.getName());
 		entity.setAuthor(book.getAuthor());
-		entity.setBuyDate(parseDate(book.getBuyDate()));
-		entity.setImgName("XXXX");
+		entity.setBuyDate(helper.parseDate(book.getBuyDate()));
+		entity.setImgName(book.getFile().getOriginalFilename());
 		bookRepo.save(entity);
 		
-		
+		try {
+			helper.saveImage(book.getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	public BookVo convertToVo(BookEntity entity) {
-		BookVo vo = new BookVo();
-		vo.setId(entity.getId());
-		vo.setName(entity.getName());
-		vo.setAuthor(entity.getAuthor());
-		vo.setBuyDate(parseDateToString(entity.getBuyDate()));
-		vo.setImgName(entity.getImgName());
-		return vo;
-	}
-	
-	public String parseDateToString(Date date) {
-	    // 定義日期格式
-	   SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	   // 將 Date 物件轉換為 String
-	   return formatter.format(date);
-	}
-	
-	/**
-	 * 轉換日期
-	 * @param dueDateStr
-	 * @return
-	 */
-	public Date parseDate(String dueDateStr) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            return formatter.parse(dueDateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-	}
+
+
 }
