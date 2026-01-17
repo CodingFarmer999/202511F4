9293d16fb675062eff7de9ec97d8493eef58fccc
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-17 11:05:01 +0800

    CORS

--- 06-restful/src/main/java/com/course/controller/RestfulController.java
+++ 06-restful/src/main/java/com/course/controller/RestfulController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.Operation;
 //@Controller
 //@ResponseBody
 @RestController
+@CrossOrigin("http://127.0.0.1:5500")
 public class RestfulController {
 
 	@Operation(tags = {"測試TAG", "測試TAG2"}, summary= "你好", description="我是描述")
