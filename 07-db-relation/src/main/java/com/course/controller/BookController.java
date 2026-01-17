446e6f8c2af487f175720bcf03afc5bfb615d1cc
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-17 12:28:23 +0800

    Dao

--- 07-db-relation/src/main/java/com/course/controller/BookController.java
+++ 07-db-relation/src/main/java/com/course/controller/BookController.java
@@ -1,8 +1,13 @@
 package com.course.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.course.service.BookService;
 
 @RestController
 public class BookController {
 
+	@Autowired
+	private BookService bookService;
 }
--- 07-db-relation/src/main/java/com/course/dao/BookDao.java
+++ 07-db-relation/src/main/java/com/course/dao/BookDao.java
@@ -1,5 +1,20 @@
 package com.course.dao;
 
-public class BookDao {
+import java.util.List;
 
+import com.course.dto.BookDto;
+
+public interface BookDao {
+
+	// Create
+	void insertBook(BookDto dto);
+	
+	// Read
+	List<BookDto> findAll();
+	
+	// Update
+	void updateBook(BookDto dto);
+	
+	// Delete
+	void deleteBookById(Long id);
 }
