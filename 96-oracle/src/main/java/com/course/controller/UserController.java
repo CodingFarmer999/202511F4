0cf8e47526b288747c54470037d28925f7010eca
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-24 16:19:43 +0800

    oracle

--- 96-oracle/src/main/java/com/course/controller/UserController.java
+++ 96-oracle/src/main/java/com/course/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.course.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.course.entity.UserEntity;
+import com.course.repository.UserRepository;
+
+@RestController
 public class UserController {
 
+	@Autowired
+	private UserRepository repo;
+	
+	@GetMapping("/users")
+	public List<UserEntity> findAll() {
+		return repo.findAll();
+	}
+	
+	@PostMapping("/user")
+	public void insertUser(UserEntity user) {
+		repo.save(user);
+	}
+	
 }
