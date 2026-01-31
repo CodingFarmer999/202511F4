aaad71c4a95ba7ef51dca0237b5af14eb8e80db1
DESKTOP-P4UUKAO\student <codingfarmer999@gmail.com>
2026-01-31 11:28:10 +0800

    Before

--- 07-db-relation/src/main/java/com/course/aspect/AopAspect.java
+++ 07-db-relation/src/main/java/com/course/aspect/AopAspect.java
@@ -1,10 +1,26 @@
 package com.course.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class AopAspect {
+	
+	Logger logger = LoggerFactory.getLogger(AopAspect.class);
 
+	@Pointcut("execution(public * com.course.service.BookService.*(..))")
+	public void pointCutMethod() {
+	    // 透由 @Pointcut 定義切點，方法內容維持空方法
+	}
+	
+	@Before("pointCutMethod()")
+	public void beforeAdvice(JoinPoint joinPoint) {
+	    logger.info("切面 AopAspect.beforeAdvice() -> @Before通知，在方法調用前先行調用，被切的方法名稱：" + joinPoint.getSignature().getName());
+	}
 }
